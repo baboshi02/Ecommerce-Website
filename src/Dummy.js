5e42adcfbe08508ff4cf0b667ca67f74837ffc20
baboshi <abdelhameid@abdelhameid>
2023-11-26 14:02:17 +0200

    ps4 added;

--- src/Dummy.js
+++ src/Dummy.js
@@ -1,24 +1,31 @@
-import iphonePhoto from './Photos/iphone.png'
-import xboxPhoto from './Photos/Xbox1.png'
-import lgPhoto from './Photos/lgTv.png'
-const Items=[
+import iphonePhoto from "./Photos/iphone.png";
+import xboxPhoto from "./Photos/Xbox1.png";
+import lgPhoto from "./Photos/lgTv.png";
+import ps4Photo from "./Photos/ps4.png";
+const Items = [
     {
-        id:1,
-        name:'Iphone',
-        price:900,
-        photo:iphonePhoto
-
-    },{
-        id:2,
-        name:'Xbox',
-        price:300,
-        photo:xboxPhoto
-
-    },{
-        id:3,
-        name:"Lg Tv",
+        id: 1,
+        name: "Iphone",
+        price: 900,
+        photo: iphonePhoto,
+    },
+    {
+        id: 2,
+        name: "Xbox",
+        price: 300,
+        photo: xboxPhoto,
+    },
+    {
+        id: 3,
+        name: "Lg Tv",
         price: 1500,
-        photo:lgPhoto
+        photo: lgPhoto,
+    },
+    {
+        id:4,
+        name:'PS4',
+        price:399,
+        photo:ps4Photo
     }
-]
-export default Items
+];
+export default Items;
