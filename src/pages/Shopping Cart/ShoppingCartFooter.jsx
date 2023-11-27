fa4b8fe31372bdd1dcc8ad3e2d49a82824dc73ca
baboshi <abdelhameid@abdelhameid>
2023-11-27 19:31:28 +0200

    Back functionality added

--- src/pages/Shopping Cart/ShoppingCartFooter.jsx
+++ src/pages/Shopping Cart/ShoppingCartFooter.jsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export const ShoppingCartFooter = (props) => {
+    const navigate=useNavigate()
     const Amount = props.Amount;
+    const buttonStyle="rounded-lg p-0.5 shadow-md bg-PrimaryColor text-SecondaryColor  hover:bg-gray-400 hover:text-gray-200   border-gray-400 hover:border-PrimaryColor mx-1"
     return (
         <div>
             <h1>
                 <b>Total Price: {Amount}$ </b>
             </h1>
             <div className="flex justify-center">
-                <button className="rounded-lg p-0.5 shadow-md bg-PrimaryColor text-SecondaryColor  hover:bg-gray-400 hover:text-gray-200   border-gray-400 hover:border-PrimaryColor mx-1">
+                <button  className={buttonStyle} onClick={()=>navigate('/')}>
                     Back
                 </button>
-                <button className=" rounded-lg p-0.5 shadow-md bg-PrimaryColor text-SecondaryColor  hover:bg-gray-400 hover:text-gray-200   border-gray-400 hover:border-PrimaryColor" >
+                <button className={buttonStyle}>
                     Proceed
                 </button>
             </div>
