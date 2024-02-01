de60e477ea26f57e4e26c986dbd9a7171eabd988
baboshi <abdelhameid@abdelhameid>
2024-02-02 00:44:21 +0200

    added final changes

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -1,14 +1,14 @@
 import Items from "../../Dummy";
 import { Product } from "./product";
 export const Shop = () => {
-    return (
-        <>
-            <h1 className="text-center text-5xl text-PrimaryColor">Shop</h1>
-            <div className="grid grid-cols-3 py-7 mx-12 my-5">
-                {Items.map((Item) => (
-                    <Product product={Item} />
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <h1 className="text-center text-5xl text-PrimaryColor">Shop</h1>
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 py-7 mx-12 my-5">
+        {Items.map((Item) => (
+          <Product product={Item} />
+        ))}
+      </div>
+    </>
+  );
 };
--- src/pages/Shopping Cart/emptyCart.jsx
+++ src/pages/Shopping Cart/emptyCart.jsx
@@ -1,17 +1,15 @@
 import { useNavigate } from "react-router-dom";
 export const EmptyCart = () => {
-    const navigate =useNavigate()
-    setTimeout(()=>{
-        navigate('/')
-    },2000)
-    return(
-        <div className="text-center text-PrimaryColor flex flex-col justify-center items-center text-lg h-28">
-            <h1>
-                {'Cart is empty'.toUpperCase() }
-            </h1>
-            <button onClick={()=>navigate('/')} className="rounded-lg p-0.5 shadow-md bg-PrimaryColor text-SecondaryColor  hover:bg-gray-400 hover:text-gray-200   border-gray-400 hover:border-PrimaryColor mx-1">
-                back
-            </button>
-        </div>
-        );
+  const navigate = useNavigate();
+  return (
+    <div className="text-center text-PrimaryColor flex flex-col justify-center items-center text-lg h-28">
+      <h1>{"Cart is empty".toUpperCase()}</h1>
+      <button
+        onClick={() => navigate("/")}
+        className="rounded-lg p-0.5 shadow-md bg-PrimaryColor text-SecondaryColor  hover:bg-gray-400 hover:text-gray-200   border-gray-400 hover:border-PrimaryColor mx-1"
+      >
+        back
+      </button>
+    </div>
+  );
 };
